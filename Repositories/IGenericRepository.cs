3f38ba4552903d214057d1b0daf814392c58855c
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-03-24 19:51:42 +0700

    Add files via upload

---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using bai_tap_Advance.Properties.Models;
+
+namespace bai_tap_Advance.Properties.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
